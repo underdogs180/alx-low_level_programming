2e9876823e5f35b1ea7a509385a148f4d6b71058
underdogs180 <asadirshad38@gmail.com>
2021-06-17 08:46:53 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-  char *grammar = "with proper grammar, but the outcome is a piece of art,";
+char *grammar = "with proper grammar, but the outcome is a piece of art,";
 
-  printf("%s\n", grammar);
-  return (0);
+printf("%s\n", grammar);
+return (0);
 }
