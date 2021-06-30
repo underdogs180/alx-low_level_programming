c5e18adbdc7db398260b5dc1ba4f0850721692fb
underdogs180 <asadirshad38@gmail.com>
2021-06-30 15:34:24 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+/**
+ * _strcat - concatanates two strings
+ * @dest: destination string
+ * @src: string to add to end of dest
+ *
+ * Return: char pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int srcLength = 0;
+	int counter = 0;
+	char *temp = dest;
+	char *tempSource = src;
+
+	while (*dest)
+		dest++;
+	while (*src++)
+		srcLength++;
+	while (counter < srcLength)
+	{
+		*dest++ = *tempSource++;
+		counter++;
+	}
+	*dest = '\0';
+	return (temp);
+}
