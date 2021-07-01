199d95eb8fa43f50ca1cc51bc34677f359fe0c35
underdogs180 <asadirshad38@gmail.com>
2021-07-01 02:30:51 -0700

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,26 +1,42 @@
-/*
- * File: 3-strcmp.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
-
 /**
- * _strcmp - Compares pointers to two strings.
- * @s1: A pointer to the first string to be compared.
- * @s2: A pointer to the second string to be compared.
+ * _strcmp - compares two strings
  *
- * Return: If str1 < str2, the negative difference of the first unmatched characters.
- *         If str1 == str2, 0.
- *         If str1 > str2, the positive difference of the first unmatched characters.
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: -15 if s1 < s2
+ *          0  if s1 = s2
+ *          15 if s1 > s2
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2)
+	int s1Length = 0, s2Length = 0;
+	int rtrnValNeg = -10;
+	int rtrnValPos = 10;
+	int smallerLength = 0;
+
+	while (*(s1 + s1Length) != '\0')
+		s1Length++;
+	while (*(s2 + s2Length) != '\0')
+		s2Length++;
+
+	smallerLength = (s1Length < s2Length) ? s1Length : s2Length;
+	rtrnValNeg += -(smallerLength);
+	rtrnValPos += smallerLength;
+
+	if (s1Length < s2Length)
+		return (rtrnValNeg);
+	if (s1Length > s2Length)
+		return (rtrnValPos);
+
+	while (*s1) /*same length, so loop through one and check both */
 	{
+		if (*s1 < *s2)
+			return (rtrnValNeg);
+		if (*s1 > *s2)
+			return (rtrnValPos);
 		s1++;
 		s2++;
 	}
-
-	return (*s1 - *s2);
+	return (0); /* equal */
 }
