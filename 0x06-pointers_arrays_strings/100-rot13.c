5b28fc92498b3db727b301217a7d14d33026a488
underdogs180 <asadirshad38@gmail.com>
2021-07-01 08:38:10 -0700

    8. rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,60 +1,27 @@
-#define rotn 13
+#include "holberton.h"
 
-char get_rot(char);
 /**
- * rot13 - encode str to rot13
+ * rot13 - encodes a string into rot13
+ * @s: string to encode
  *
- * @str: string to convert
- *
- * Return: pointer to str
+ * Return: address of s
  */
-char *rot13(char *str)
+char *rot13(char *s)
 {
-  char temp;
-  int i = 0;
+  int i, j;
+  char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-  while (str[i] != '\0')
+  for (i = 0; *(s + i); i++)
     {
-      temp = get_rot(str[i]);
-      if (temp != 0)
-	str[i] = temp;
-      i++;
+      for (j = 0; j < 52; j++)
+	{
+	  if (a[j] == *(s + i))
+	    {
+	      *(s + i) = b[j];
+	      break;
+	    }
+	}
     }
-  return (str);
-}
-/**
- * get_rot - gets leet character for english char
- *
- * @conv: english character to convert/check
- *
- * Return: char rot13 representation
- */
-char get_rot(char conv)
-{
-  char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
-		     'h', 'i', 'j', 'k', 'l', 'm', 'n',
-		     'o', 'p', 'q', 'r', 's', 't', 'u',
-		     'v', 'w', 'x', 'y', 'z',
-		     'A', 'B', 'C', 'D', 'E', 'F', 'G',
-		     'H', 'I', 'J', 'K', 'L', 'M', 'N',
-		     'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-		     'V', 'W', 'X', 'Y', 'Z', 0};
-  int rotTranslate[] = { rotn, rotn, rotn, rotn, rotn,
-			 rotn, rotn, rotn, rotn, rotn,
-			 rotn, rotn, rotn, -(rotn), -(rotn),
-			 -(rotn), -(rotn), -(rotn), -(rotn), -(rotn),
-			 -(rotn), -(rotn), -(rotn), -(rotn), -(rotn),
-			 -(rotn), /* end lowercase */
-			 rotn, rotn, rotn, rotn, rotn,
-			 rotn, rotn, rotn, rotn, rotn,
-			 rotn, rotn, rotn, -(rotn), -(rotn),
-			 -(rotn), -(rotn), -(rotn), -(rotn), -(rotn),
-			 -(rotn), -(rotn), -(rotn), -(rotn), -(rotn),
-			 -(rotn), 0};
-  int arrSize = 26 * 2;
-  int i = 0;
-
-  while (conv != alphabet[i] && i < arrSize)
-    i++;
-  return (alphabet[i] + rotTranslate[i]);
+  return (s);
 }
