177c1db39a3274600dd5d8a3a533d741c76f2ccf
underdogs180 <asadirshad38@gmail.com>
2021-07-13 06:10:01 -0700

    0x0B

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -11,45 +11,45 @@ char *get_next_word(char *str);
  */
 char **strtow(char *str)
 {
-  char **words;
-  int wc, wordLen, n, i = 0;
+char **words;
+int wc, wordLen, n, i = 0;
 
-  if (str == NULL || !*str)
-    return (NULL);
-  wc = get_word_count(str);
-  if (wc == 0)
-    return (NULL);
-  words = malloc((wc + 1) * sizeof(char *));
-  if (words == NULL)
-    return (NULL);
-  while (i < wc)
-    {
-      wordLen = get_word_length(str);
-      if (*str == ' ' || *str == '\t')
-	str = get_next_word(str);
-      words[i] = malloc((wordLen + 1) * sizeof(char));
-      if (words[i] == NULL)
-	{
-	  while (i >= 0)
-	    {
-	      i--;
-	      free(words[i]);
-	    }
-	  free(words);
-	  return (NULL);
-	}
-      n = 0;
-      while (n < wordLen)
-	{
-	  words[i][n] = *(str + n);
-	  n++;
-	}
-      words[i][n] = '\0'; /* set end of str */
-      str = get_next_word(str);
-      i++;
-    }
-  words[i] = NULL; /* last element is null for iteration */
-  return (words);
+if (str == NULL || !*str)
+return (NULL);
+wc = get_word_count(str);
+if (wc == 0)
+return (NULL);
+words = malloc((wc + 1) * sizeof(char *));
+if (words == NULL)
+return (NULL);
+while (i < wc)
+{
+wordLen = get_word_length(str);
+if (*str == ' ' || *str == '\t')
+str = get_next_word(str);
+words[i] = malloc((wordLen + 1) * sizeof(char));
+if (words[i] == NULL)
+{
+while (i >= 0)
+{
+i--;
+free(words[i]);
+}
+free(words);
+return (NULL);
+}
+n = 0;
+while (n < wordLen)
+{
+words[i][n] = *(str + n);
+n++;
+}
+words[i][n] = '\0'; /* set end of str */
+str = get_next_word(str);
+i++;
+}
+words[i] = NULL; /* last element is null for iteration */
+return (words);
 }
 /**
  * get_word_length - gets the word length of cur word in str
@@ -60,21 +60,21 @@ char **strtow(char *str)
  */
 int get_word_length(char *str)
 {
-  int wLen = 0, pending = 1, i = 0;
+int wLen = 0, pending = 1, i = 0;
 
-  while (*(str + i))
-    {
-      if (*(str + i) == ' ' || *(str + i) == '\t')
-	pending = 1;
-      else if (pending)
-	{
-	  wLen++;
-	}
-      if (wLen > 0 && (*(str + i) == ' ' || *(str + i) == '\t'))
-	break;
-      i++;
-    }
-  return (wLen);
+while (*(str + i))
+{
+if (*(str + i) == ' ' || *(str + i) == '\t')
+pending = 1;
+else if (pending)
+{
+wLen++;
+}
+if (wLen > 0 && (*(str + i) == ' ' || *(str + i) == '\t'))
+break;
+i++;
+}
+return (wLen);
 }
 /**
  * get_word_count - gets the word count of a string
@@ -85,20 +85,20 @@ int get_word_length(char *str)
  */
 int get_word_count(char *str)
 {
-  int wc = 0, pending = 1, i = 0;
+int wc = 0, pending = 1, i = 0;
 
-  while (*(str + i))
-    {
-      if (*(str + i) == ' ' || *(str + i) == '\t')
-	pending = 1;
-      else if (pending)
-	{
-	  pending = 0;
-	  wc++;
-	}
-      i++;
-    }
-  return (wc);
+while (*(str + i))
+{
+if (*(str + i) == ' ' || *(str + i) == '\t')
+pending = 1;
+else if (pending)
+{
+pending = 0;
+wc++;
+}
+i++;
+}
+return (wc);
 }
 /**
  * get_next_word - gets the next word in a string
@@ -109,16 +109,16 @@ int get_word_count(char *str)
  */
 char *get_next_word(char *str)
 {
-  int pending = 0;
-  int i = 0;
+int pending = 0;
+int i = 0;
 
-  while (*(str + i))
-    {
-      if (*(str + i) == ' ' || *(str + i) == '\t')
-	pending = 1;
-      else if (pending)
-	break;
-      i++;
-    }
-  return (str + i);
+while (*(str + i))
+{
+if (*(str + i) == ' ' || *(str + i) == '\t')
+pending = 1;
+else if (pending)
+break;
+i++;
+}
+return (str + i);
 }
