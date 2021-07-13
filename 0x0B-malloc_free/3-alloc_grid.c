70b3784dbb16aa0992a50ae28082bedef33e5abb
underdogs180 <asadirshad38@gmail.com>
2021-07-13 03:50:06 -0700

    0x0B

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,34 +1,47 @@
-#include <stdio.h>
+#include "holberton.h"
 #include <stdlib.h>
-/**
- * str_concat - concatenates two strings, makes new string
- *
- * @s1: string 1 to concatenate with string 2
- * @s2: string 2 to concatenate with string 1
- *
- * Return: pointer to new string, or NULL on failure
- */
-char *str_concat(char *s1, char *s2)
-{
-int s1Size = 0, s2Size = 0;
-char *conc, *concStart;
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-while (*(s1 + s1Size))
-s1Size++;
-while (*(s2 + s2Size))
-s2Size++;
-conc = malloc(sizeof(char) * (s1Size + s2Size + 1));
-if (conc == NULL)
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: width of array
+ * @height: height of array
+ * Return: double pointer to multi-dimen array
+ */
+int **alloc_grid(int width, int height)
+{
+int **grid, r, c;
+
+if (width <= 0 || height <= 0)
+{
 return (NULL);
-concStart = conc;
-while (*s1)
-*conc++ = *s1++;
-while (*s2)
-*conc++ = *s2++;
-*conc = '\0';
-return (concStart);
+}
+
+grid = malloc(sizeof(int *) * height);
+/*if memory is insifficuent*/
+if (!grid)
+return (NULL);
+
+c = 0;
+while (c < height)
+{
+*(grid + c) = malloc(width * sizeof(int));
+
+if (!(*(grid + c)))
+{
+while (c--)
+free(*(grid + c));
+free(grid);
+return (NULL);
+}
+r = 0;
+while (r < width)
+{
+*(*(grid + c) + r) = 0;
+r++;
+
+}
+c++;
+}
+
+return (grid);
 }
