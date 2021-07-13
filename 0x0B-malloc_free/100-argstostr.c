f1f146419aeaa809314124bb2bcd47c1a9fb7816
underdogs180 <asadirshad38@gmail.com>
2021-07-13 05:47:13 -0700

    0x0B

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,40 +1,41 @@
-#include "holberton.h"
 #include <stdlib.h>
 
 /**
- * argstostr - concatenates all the arguments of your program.
- * @ac: number of arguments
- * @av: double pointer to arguments
- *
- * Return:pointer to new string, or NULL if error
+ *argstostr - concatenates all the arguments of your program.
+ *@ac: argc size double pointer
+ *@av: argv parameter double pointer
+ *Return: new strins by pointer
  */
 char *argstostr(int ac, char **av)
 {
-int i, j, k = 0, n = 0;
-char *s;
+  char *ptr;
+  int i = 0, j = 0, k = 0;
 
-if (ac <= 0 || av == NULL)
-return (NULL);
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j]; j++)
-n++;
-n++;
-}
-n++;
-s = malloc(n * sizeof(char));
-if (s == NULL)
-return (NULL);
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j]; j++)
-{
-s[k] = av[i][j];
-k++;
-}
-s[k] = '\n';
-k++;
-}
-s[k] = '\0';
-return (s);
+  if (ac == 0 || av == NULL)
+    return (NULL);
+
+  for (i = 0; i < ac; i++)
+    {
+      for (j = 0; av[i][j]; j++)
+	k++;
+    }
+
+  ptr = malloc(sizeof(char) * (ac + k + 1));
+  if (!(ptr == NULL))
+    {
+      k = 0;
+      for (i = 0; i < ac; i++)
+	{
+	  for (j = 0; av[i][j]; j++)
+	    {
+	      ptr[k] = av[i][j];
+	      k++;
+	    }
+	  ptr[k] = '\n';
+	  k++;
+	}
+      return (ptr);
+    }
+  else
+    return (NULL);
 }
