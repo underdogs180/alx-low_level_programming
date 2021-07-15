8051108932612fe6b162c9603933bf867d361daf
underdogs180 <asadirshad38@gmail.com>
2021-07-15 14:01:42 -0700

    0x0C

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,10 +7,10 @@
  */
 void *malloc_checked(unsigned int b)
 {
-  void *ptr = malloc(b);
+void *ptr = malloc(b);
 
-  if (ptr == NULL)
-    exit(98);
+if (ptr == NULL)
+exit(98);
 
-  return (ptr);
+return (ptr);
 }
