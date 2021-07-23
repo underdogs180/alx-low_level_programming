33690b73e3e2827b28e41e5e394234eaa79728ea
underdogs180 <asadirshad38@gmail.com>
2021-07-23 03:21:48 -0700

    0x10

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,47 +1,36 @@
 #include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
 /**
- * print_strings - prints all strings given, variadic
- * @seperator: seperator to print between strings
- * @n: number of strings
- * @...: strings to print
+ * print_strings - Print strings with a separator.
+ * @separator: Separator
+ * @n: Quantity of numbers
  *
- * Return: always void
+ * Return: Result of adding the numbers or 0 if not arguments were passed
  */
-void print_strings(const char *seperator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-va_list strs;
-unsigned int i;
-char *cur_str;
-const char *sep_cp;
-char *nil, *nil_cp;
+unsigned int i = 0;
+va_list ap;
+char *p;
 
-nil = "(nil)";
-va_start(strs, n);
-
+va_start(ap, n);
 for (i = 0; i < n; i++)
 {
-cur_str = va_arg(strs, char *);
-if (cur_str == NULL) /* null current string */
+p = va_arg(ap, char *);
+if (p == NULL)
 {
-nil_cp = nil;
-while (*nil_cp)
-putchar(*nil_cp++)
+printf("(nil)");
 }
 else
 {
-while (*cur_str)
+printf("%s", p);
+}
+if (separator != NULL && i < (n - 1))
 {
-putchar(*cur_str++);
+printf("%s", separator);
 }
 }
-/* seperate strings */
-if (seperator == NULL || *seperator == '\0')
-continue;
-sep_cp = seperator;
-while ((*sep_cp)&&(i < (n - 1)))
-putchar(*sep_cp++);
-}
-putchar('\n');
+printf("\n");
+va_end(ap);
 }
