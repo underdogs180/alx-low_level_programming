2239f236401bea045d3ce52bb8e4992ea1c92970
underdogs180 <asadirshad38@gmail.com>
2021-07-29 08:22:34 -0700

    0x12

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,11 +10,11 @@
  */
 unsigned int _strlen(char *str)
 {
-  unsigned int i;
+unsigned int i;
 
-  for (i = 0; str[i]; i++)
-    ;
-  return (i);
+for (i = 0; str[i]; i++)
+;
+return (i);
 }
 
 /**
@@ -26,21 +26,21 @@ unsigned int _strlen(char *str)
  */
 list_t *add_node(list_t **head, const char *str)
 {
-  list_t *new;
+list_t *new;
 
-  if (str == NULL)
-    return (NULL);
-  new = malloc(sizeof(list_t));
-  if (new == NULL)
-    return (NULL);
-  new->str = strdup(str);
-  if (new->str == NULL)
-    {
-      free(new);
-      return (NULL);
-    }
-  new->len = _strlen(new->str);
-  new->next = *head;
-  *head = new;
-  return (new);
+if (str == NULL)
+return (NULL);
+new = malloc(sizeof(list_t));
+if (new == NULL)
+return (NULL);
+new->str = strdup(str);
+if (new->str == NULL)
+{
+free(new);
+return (NULL);
+}
+new->len = _strlen(new->str);
+new->next = *head;
+*head = new;
+return (new);
 }
