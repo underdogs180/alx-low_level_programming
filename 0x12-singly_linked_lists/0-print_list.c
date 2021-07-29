bf5d66ec20930f5b1d011ead918ba62d365253c6
underdogs180 <asadirshad38@gmail.com>
2021-07-29 07:41:05 -0700

    0x12

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: Listed lists
+ * Return: counter
+ */
+size_t print_list(const list_t *h)
+{
+unsigned int count = 0;
+
+while (h)
+{
+count++;
+h->str ? printf("[%d] %s\n", h->len, h->str) :
+printf("[0] (nil)\n");
+h = h->next;
+}
+
+return (count);
+}
