36e2905f16969461ff30b7ed9ed24963b113ab38
underdogs180 <asadirshad38@gmail.com>
2021-08-01 23:53:00 -0700

    0x13

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,14 +8,14 @@
  */
 size_t listint_len(const listint_t *h)
 {
-  size_t n_nodes = 0;
-  const listint_t *temp_h;
+size_t n_nodes = 0;
+const listint_t *temp_h;
 
-  temp_h = h;
-  while (temp_h)
-    {
-      temp_h = temp_h->next;
-      n_nodes++;
-    }
-  return (n_nodes);
+temp_h = h;
+while (temp_h)
+{
+temp_h = temp_h->next;
+n_nodes++;
+}
+return (n_nodes);
 }
