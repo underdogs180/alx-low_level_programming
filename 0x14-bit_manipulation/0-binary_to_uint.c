16d0be52f76a98783abd294c4172ea16ddbcca1e
underdogs180 <asadirshad38@gmail.com>
2021-08-05 01:22:46 -0700

    0x14

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -20,7 +20,7 @@ while (len > 0)
 if (b[len - 1] != '0' && b[len - 1] != '1')
 return (0);
 temp = (b[len - 1] == '1') ? 1 : 0;
-result += (temp * base);
+result += (temp  *base);
 base *= 2;
 len--;
 }
