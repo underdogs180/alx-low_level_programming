25c55989c5753e14efaa081631ce9877c0caa8f0
underdogs180 <asadirshad38@gmail.com>
2021-08-05 01:38:16 -0700

    0x14

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,28 +1,35 @@
 #include "holberton.h"
+#include <stdlib.h>
+
 /**
- * binary_to_uint - converts binary string to an unsigned integer
- * @b: pointer to binary string in memory to convert to integer
+ * binary_to_uint - Convert a string representing a binary number to an
+ * unsigned int decimal value
+ * @b: The string containing the binary
  *
- * Return: unsigned integer representing binary value
+ * Return: 0 if string contains something other than 1 or 0, or
+ * if the string is NULL, return the decimal value on success
  */
 unsigned int binary_to_uint(const char *b)
 {
-int base = 1;
-unsigned int temp = 0, result = 0;
-int len = 0;
+unsigned int i, len;
+unsigned int decval;
 
-if (!b)
+i = len = decval = 0;
+if (b == NULL)
 return (0);
-while (b[len])
-len++;
-while (len > 0)
+while (b[len] != '\0')
 {
-if (b[len - 1] != '0' && b[len - 1] != '1')
+if (b[len] == '0' || b[len] == '1')
+len++;
+else
 return (0);
-temp = (b[len - 1] == '1') ? 1 : 0;
-result += (temp  *base);
-base *= 2;
-len--;
 }
-return (result);
+while (i < len)
+{
+decval = decval << 1;
+if (b[i] == '1')
+decval += 1;
+i++;
+}
+return (decval);
 }
