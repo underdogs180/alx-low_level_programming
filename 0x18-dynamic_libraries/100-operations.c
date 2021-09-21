9b51f2955d70086901ef670af84bb71f5465d0a2
underdogs180 <asadirshad38@gmail.com>
2021-09-21 00:05:53 -0700

    100-operations

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  * add - adds two integer
+ *   * @a: int type number
+ *    * @b: int type number
+ *     * Return: sucessfully
+ *      */
+int add(int a, int b)
+{
+		return (a + b);
+}
+
+/**
+ *  * sub - subtract two integers
+ *   * @a: int type number
+ *    * @b: int type number
+ *     * Return: sucessfully
+ *      */
+
+int sub(int a, int b)
+{
+		return (a - b);
+}
+
+/**
+ *  * mul - multiply two integers
+ *   * @a: int type number
+ *    * @b: int type number
+ *     * Return: sucessfully
+ *      */
+int mul(int a, int b)
+{
+		return (a * b);
+}
+
+/**
+ *  * div - divide two integers
+ *   * @a: int type number
+ *    * @b: int type number
+ *     * Return: sucessfully
+ *      */
+int div(int a, int b)
+{
+		return (a / b);
+}
+
+/**
+ *  * mod - divides  two integers
+ *   * @a: int type number
+ *    * @b: int type number
+ *     * Return: sucessfully
+ *      */
+int mod(int a, int b)
+{
+		return (a % b);
+}
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,6 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -7,7 +8,7 @@ int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void _puts(char *str);
+void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
@@ -20,4 +21,11 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif /* HOLBERTON_H */
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif
